f28959392354d3022f5482e0b32274aa0c41bffd
Eugene Marion Jennings <friendlysquidhacker@gmail.com>
2016-07-25 17:18:05 -0400

    Starting work on a poetry quoting bot

---
+++ quotepoetry.py
@@ -0,0 +1,41 @@
+from __future__ import absolute_import, division, print_function, unicode_literals
+
+try:
+	from html.parser import HTMLParser
+	version = 'py3'
+except:
+	from HTMLParser import HTMLParser
+	version = 'py2'
+finally:
+	class scraper(HTMLParser):
+		
+
+try:
+	pass
+except Exception, e:
+	raise e
+finally:
+	pass
+
+class 
+# -- How do I quote poetry?
+# 1: Open the URL to a poetry website
+# 2: Search for love poems
+# 3: Open the first result returned
+# 4: Grab the text
+# 5: Regurgitate it line by line.
+
+# Step one: Open the URL to the poetry website.
+
+
+
+
+
+
+
+
+
+#poem = open('poem.txt')
+#print(" ")
+#for line in poem:
+#	print(line, end=' ')
